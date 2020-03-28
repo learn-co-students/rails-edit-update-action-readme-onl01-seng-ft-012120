83d0378c302291228d5f73b34daf8113f8bacb8d
cosmicChimp <ian.t.marrs@gmail.com>
2020-03-28 00:34:01 -0600

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,11 @@
+
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
 
   def show
+    # raise params.inspect
     @article = Article.find(params[:id])
   end
 
@@ -16,6 +18,16 @@ class ArticlesController < ApplicationController
     @article.title = params[:title]
     @article.description = params[:description]
     @article.save
+    redirect_to article_path(@article)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(title: params[:article][:title], description: params[:article][:description])
     redirect_to article_path(@article)
   end
 
